e3c87e66b60ce90a680ce9c02af42910eed33bed
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-08-17 09:48:00 -0500

    scss

---
+++ server.js
@@ -0,0 +1,10 @@
+//before starting, changes were made to package.json to include sass.
+
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+app.listen(7000, () => {
+  console.log('listening on port 7000');
+});
