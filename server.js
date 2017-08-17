e1c82d583d8a21a30e5cac7d4fa3178e8a0cef09
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-08-17 10:59:39 -0500

    running

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 //before starting, changes were made to package.json to include sass.
-
+//changes were made to
 const express = require('express');
+const mustacheExpress = require('mustache-express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb://localhost:27017/people', {
+  useMongoClient: true
+});
+
+require('dotenv').config();
+
+const Person = require('./models/Person');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+console.log(process.env.NODE_ENV);
+
+const mustache = mustacheExpress();
+if (process.env.NODE_ENV === 'development') {
+  mustache.cache = null;
+}
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
 app.use(express.static('public'));
 
-console.log('===============')
-console.log('process.env');
+
+app.post('/addperson', (req, res) => {
+  //save in mongoose
+  const person = new Person();
+  person.firstName = req.body.firstName;
+  person.lastName = req.body.lastName;
+  person.save((err, person) => {
+    console.log('I created', person);
+    res.redirect('/');
+  });
+
+  //redirect back to index page
+});
+
+app.get('/people', (req, res) => {
+  Person.find({}, (err, people) => {
+    res.json(people);
+  });
+});
+
+
+
 app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.port}`);
+  console.log(`Listening on port ${process.env.PORT}`);
 });
